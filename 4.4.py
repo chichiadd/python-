772a99632db93e9a747db077a0fc231236728d79
adchichi <chichi2372278420@foxmail.com>
2024-04-04 22:46:55 +0800

    4.4

--- 4.4.py
+++ 4.4.py
@@ -1,1 +1,13 @@
 #hello world!
+#json练习
+#列表、字典转换json
+import json
+
+data1 = [{"name":"拉芙塔莉雅"},{"age":14}]
+data2 = {"name":"Rapgtalia","AGE":14}
+json_str1 = json.dumps(data1)
+json_str2 = json.dumps(data1,ensure_ascii=False)
+print(json_str1)
+print(json_str2)
+str1 = json.loads(json_str1)
+print(str1)
